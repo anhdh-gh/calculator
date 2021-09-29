f520e0a79ae9fafff9f219fa8e0d2d5b6cb92e2d
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2021-09-30 02:27:38 +0700

    Update check isNumberic

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 export const isNumeric = (str) => {
     if (typeof str != "string") return false // we only process strings!  
+    if(str.includes('Infinity')) return false
     return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
            !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
