0493fcfcfb975279af5dac8e096202b92e4ce925
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2021-09-29 17:26:47 +0700

    Push algorithm

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,125 @@
 import '../assets/styles/Calculator.css'
+import { useState, useRef } from 'react'
+import { isNumeric, calculate } from '../utils'
+
+let entering = true, number1 = "", number2 = "", operator = ""
 
 const Calculator = (props) => {
+
+    const [screen, setScreen] = useState("")
+    const [input, setInput] = useState("")
+    const refInput = useRef()
+
+    // Các hàm xử lý
+    const reset = () => {
+        setInput("")
+        setScreen("")
+        number1 = ""
+        number2 = ""
+        operator = ""
+        entering = true
+    }
+
+    const addNumber = (number) => {
+        if(entering === true) setInput(input + number)
+        else {
+            if(screen.includes("=") || !isNumeric(number)) reset()
+            setInput(number)
+            entering = true
+        }
+        refInput.current.scrollLeft = refInput.current.scrollWidth
+    }
+
+    const backHandler = () => {
+        if(input.length !== 0 && entering === true) {
+            if(input.length === 2 && input.charAt(0) === "-") setInput("")
+            else setInput(input.substring(0, input.length - 1))
+        } 
+    }
+
+    const amHandler = () => {
+        if(input.length !== 0 && entering === true) {
+            const c = input.charAt(0)
+            if(c === '-') setInput(input.substring(1))
+            else setInput("-" + input)
+        }
+        refInput.current.scrollLeft = 0
+    }
+
+    const dotHandler = () => {
+        if(input.length !== 0 && entering === true) {
+            const c = input.charAt(input.length - 1)
+            if(c !== "-" && !input.includes(".")) setInput(input + ".");
+        }
+    }
+
+    const addOperator = (operatorInput) => {
+        if(input.length !== 0 && isNumeric(input)) {
+            if(screen.includes("=")) number1 = input
+            if(entering === true) {
+                if(number1.length === 0) number1 = input
+                else {
+                    number2 = input
+                    number1 = calculate(number1, number2, operator).toString()
+                }
+                entering = false
+            }
+            setInput(number1)
+            setScreen(number1 + " " + operatorInput)
+            operator = operatorInput
+        }
+    }
+
+    const bangHandler = () => {
+        if(input.length !== 0 && number1.length !== 0 && operator.length !== 0 && isNumeric(input)) {
+            number2 = input
+            if(screen.includes("=")) {
+                setScreen(number2 + " " + operator + " " + number1 + " =")
+                setInput(calculate(number2, number1, operator).toString())
+            }
+            else {
+                setScreen(number1 + " " + operator + " " + number2 + " =")
+                setInput(calculate(number1, number2, operator).toString())
+                number1 = number2
+            }
+            entering = false
+        }
+    }
+
+    // Render giao diện
     return <div className="calculator-container">
         {/* Phần hiển thị phép tính */}
-        <input type="text" className="calculator-screen" readOnly></input>
+        <input type="text" className="calculator-screen" value={screen} readOnly/>
 
         {/* Phần nhập số */}
-        <input type="text" className="calculator-input" readOnly></input>
+        <input type="text" ref={refInput} className="calculator-input" value={input} readOnly/>
 
         {/* Phần các nút ấn */}
         <div className="calculator-buttons">
-            <button type="button" className="function">OFF</button>
-            <button type="button" className="function">AC</button>
-            <button type="button" className="number">⬅</button>
-            <button type="button" className="operator">÷</button>
+            <button type="button" className="function noselect">OFF</button>
+            <button type="button" className="function noselect" onClick={reset}>AC</button>
+            <button type="button" className="number noselect" onClick={backHandler}>⬅</button>
+            <button type="button" className="operator noselect" onClick={() => addOperator('÷')}>÷</button>
 
-            <button type="button" className="number">7</button>
-            <button type="button" className="number">8</button>
-            <button type="button" className="number">9</button>
-            <button type="button" className="operator">×</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('7')}>7</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('8')}>8</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('9')}>9</button>
+            <button type="button" className="operator noselect" onClick={() => addOperator('×')}>×</button>
 
-            <button type="button" className="number">4</button>
-            <button type="button" className="number">5</button>
-            <button type="button" className="number">6</button>
-            <button type="button" className="operator">–</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('4')}>4</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('5')}>5</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('6')}>6</button>
+            <button type="button" className="operator noselect" onClick={() => addOperator('–')}>–</button>
 
-            <button type="button" className="number">1</button>
-            <button type="button" className="number">2</button>
-            <button type="button" className="number">3</button>
-            <button type="button" className="operator">+</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('1')}>1</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('2')}>2</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('3')}>3</button>
+            <button type="button" className="operator noselect" onClick={() => addOperator('+')}>+</button>
 
-            <button type="button" className="number">+/-</button>
-            <button type="button" className="number">0</button>
-            <button type="button" className="number">•</button>
-            <button type="button" className="primary">=</button>
+            <button type="button" className="number noselect" onClick={amHandler}>+/-</button>
+            <button type="button" className="number noselect" onClick={() => addNumber('0')}>0</button>
+            <button type="button" className="number noselect" onClick={dotHandler}>•</button>
+            <button type="button" className="primary noselect" onClick={bangHandler}>=</button>
         </div>
     </div>
 }
---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+export const isNumeric = (str) => {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+export const calculate = (number1, number2, operator) => {
+    const n1 = parseFloat(number1)
+    const n2 = parseFloat(number2)
+    switch(operator) {
+        case "+":
+            return n1 + n2
+        case "–":
+            return n1 - n2
+        case "×":
+            return n1 * n2
+        case "÷":
+            return n1 / n2
+        default:
+            return 0
+    }    
+}
